1f0a777630a3e1bcb52150a90b22bbb46fff8512
Nitinyadav237 <nitin.yadav.ny237@gmail.com>
2026-02-05 16:19:25 +0000

    Squashed 'escrow_litesvm/' content from commit eb030f5
    git-subtree-dir: escrow_litesvm
    git-subtree-split: eb030f593bc09472f38e3b46479803172e3e1394

---
+++ programs/anchor-escrow/src/lib.rs
@@ -0,0 +1,39 @@
+#![allow(unexpected_cfgs)]
+#![allow(deprecated)]
+
+use anchor_lang::prelude::*;
+
+mod instructions;
+mod state;
+mod tests;
+
+use instructions::*;
+
+declare_id!("FircrADQ2wgGuvpm8qneNCfKM7o5zoHTWnDQxngpTQ3J");
+
+#[program]
+pub mod anchor_escrow {
+    use super::*;
+
+    pub fn make(
+        ctx: Context<Make>,
+        seed: u64,
+        deposit: u64,
+        receive: u64,
+        waiting_time: i64,
+    ) -> Result<()> {
+        ctx.accounts
+            .init_escrow(seed, receive, waiting_time, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.check_waiting_time()?;
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
+    }
+}
